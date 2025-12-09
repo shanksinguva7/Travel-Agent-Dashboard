02445ec659ac0f01cdcbda71ceecffe717ce3227
Shashank Inguva <shashank.inguva@outlook.in>
2025-12-09 22:17:03 +0530

    Routing and some other changes

---
+++ Travel Agent Dashboard/app/routes/admin/Adminlayout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Outlet} from "react-router";
+
+const Adminlayout = () => {
+  return (
+    <div className="admin-layout">
+      MobileSidebar 
+      <aside className="w-full max-w-[270px] hidden lg:block">Sidebar</aside>
+
+      <aside className="children">
+        <Outlet />
+      </aside>
+    </div>
+  )
+}
+
+export default Adminlayout
